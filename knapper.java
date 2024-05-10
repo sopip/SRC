4beb821f401dfbfcda8ef327ac78554498adf193
sopop <162905162+sopip@users.noreply.github.com>
2024-05-10 10:37:23 +0200

    lorte pc

--- knapper.java
+++ knapper.java
@@ -1,4 +1,75 @@
 //knapper
-//import processing.core.PApplet;
+import processing.core.*;
+class Knap{
+  int x, y, w, h;
+  String titel;
+  
+  Knap(int xin, int yin, int win, int hin, String titelin){ // in for input så det ikke bliver forvirrende med ens navne
+    x = xin;
+    y = yin;
+    w = win;
+    h = hin;
+    titel = titelin;
+  }
+  
+  // funktionalitet
+  boolean knapKlik(int xin, int yin) {
+    boolean hitY = xin > x && xin < x + w;
+    boolean hitX = yin > y && yin < y + h;    
+    return hitX && hitY;
+  }
+  
+  void tegnKnap() {
+    fill(255);
+    rect(x,y,w,h);
+    textSize(20);
+    fill(0);
+    text(titel,x+10,y+h/2);
+  } 
+}
 
-//import processing.core.PVector;
+class Volumen {
+  int x, y, b, h;
+
+
+  Volumen(int xin, int yin, int bin, int hin){
+    x = xin;
+    y = yin;
+    b = bin;
+    h = hin;
+  }
+  
+  void tegnVolumen(){
+    rect(x,y,b,h);
+  }
+  
+  void ændrVol(int v){
+    b = b + v;
+  }
+  // funktionalitet
+  void resetknap(){
+  b=100;
+  }
+}
+
+
+
+
+/*
+public class button(){
+  private int a;
+  private int b;
+  private int c;
+  private int d;
+  private int e;
+  private int f;
+  
+  public button(PApplet p, int ){
+
+
+  }
+*/
+
+
+
+
--- src.pde
+++ src.pde
@@ -1,7 +1,15 @@
-
+Knap plusKnap, minusKnap, resetKnap;
+Volumen vol; 
 
 void setup(){
 size(1500,1000);
+public int[]talvardi= {1,2,3,9,0,4,5,8,7,6};
+
+
+  plusKnap = new Knap(10,10,100,50,"Plus");
+  minusKnap = new Knap(10,70,100,50,"Minus");
+  resetKnap = new Knap(10,130,100,50,"Reset");
+  vol = new Volumen(fiat500,120,10,120,50);
 
 }
 
@@ -9,4 +17,18 @@ void draw(){
 background(0);
 
 
+  plusKnap.tegnKnap();
+  minusKnap.tegnKnap();
+  resetKnap.tegnKnap();
+  vol.tegnVolumen();
+  if(mousePressed && plusKnap.knapKlik(mouseX,mouseY)){
+    vol.ændrVol(1);
+  }
+  if(mousePressed && minusKnap.knapKlik(mouseX,mouseY)){
+    vol.ændrVol(-1);
+  }
+  if(mousePressed && resetKnap.knapKlik(mouseX,mouseY)){ 
+   vol.resetknap();
+  
+ }
 }
